b74fa2c61586b296f29191c7e38c070bef04c291
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2024-04-08 22:33:29 -0700

    Finishing touches

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
         main: resolve(__dirname, 'src/index.html'),
         edit: resolve(__dirname, 'src/edit.html'),
         delete: resolve(__dirname, 'src/delete.html'),
-        create: resolve(__dirname, 'src/create.html')
+        create: resolve(__dirname, 'src/create.html'),
+        about: resolve(__dirname, 'src/about.html')
 
       },
     },
